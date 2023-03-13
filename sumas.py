342294c15a4abe1908475eda73eb6f75137f91a6
cprofuckit <114919521+cprofuckit@users.noreply.github.com>
2023-03-13 13:42:35 +0100

    Add files via upload

---
+++ sumas.py
@@ -0,0 +1,4 @@
+num1= int (input("dime un número: "))
+num2= int (input("dime otro número: "))
+res=num1+num2
+print("El resultado es: ",res)
---
+++ temperatura.py
@@ -0,0 +1,8 @@
+temp=int(input("Dime la temperatura: "))
+                
+if(temp >=0):                
+    print("es agua")
+elif(temp <=0):
+    print("es hielo")
+elif(temp >=50):
+    print("es gas")
